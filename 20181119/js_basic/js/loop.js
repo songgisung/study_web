d6c67a9f3416522e32eee39ecc852a3b156bc38e
songgisung <sgsgisung@gmail.com>
2018-11-19 17:49:08 +0900

    20181119

---
+++ 20181119/js_basic/js/loop.js
@@ -0,0 +1,31 @@
+// 繰り返し処理
+
+// 例題 for
+// 変数iの値を1から3まで出力するプログラムを、
+// for文を使って記述
+for (var i = 1; i <4; i++) {
+  console.log(i);
+}
+
+
+
+// 例題 while
+// 変数 jの値を1から3まで出力するプログラムを、
+// while文を使って記述
+var sum = 1;
+while(sum <= 3){
+  console.log(sum);
+  sum++;
+
+}
+
+// 例題 do while
+// 例題
+// 変数kの値を1から3までコンソールに出力するプログラムを、
+// do while文を使って記述する
+sum=1;
+do {
+
+  console.log(sum);
+  sum++;
+} while (sum <= 3);
